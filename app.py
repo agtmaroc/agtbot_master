3a7727c8f18e643dafe61c65df9f26d2333a41ac
agtmaroc <84507113+agtmaroc@users.noreply.github.com>
2021-05-20 13:23:28 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,62 +1,70 @@
-from flask import Flask, request, Response
-import requests, json, random, os
+
+from typing import Text
+from flask import Flask, json, request
+from pymessenger import Bot
+#####################################################
+import json
+import os
+from os import environ
+import dialogflow
+from google.api_core.exceptions import InvalidArgument
+######################################################
+
 app = Flask(__name__)
+PAGE_ACCESS_TOKEN = 'EAACBNZAZAIEZBEBABhai7xJ2GB7orSr6BPvTtDglSnZBSLNGmhbyvyvvHWVhiLRhuQXgxBLZChjWSTMdM4mSGTvMPY3t61NK3tBRTYkrEXJmGBiQoXdZBZCTEu859OvIgl8zvVVvpLhHzSla404xiCQa6moVElXjHZBBkRuv1sp7LUytoCNghbt8'
 
-# env_variables
-# token to verify that this bot is legit
-verify_token = 'UNIQE_TOKEN' #os.getenv('VERIFY_TOKEN', None)
-# token to send messages through facebook messenger
-access_token = 'EAACRZB81tqu0BANSFYFaLgoLDCnWS3cZCOhln9c1uyh3Y8oMCtqJvveAW86f505BcYYKfGoyIoZA5TUOnQm5Vlgzm5DQU4UAsuQSDEsTYcLTZCt9bhOQo3krhXYzFwVc8U1jTQg7ltNWs9Atmkt1K0k43xSRD0SCZASA2DzSHkFndmCgVGN3p'#os.getenv('ACCESS_TOKEN', None)
+bot = Bot(PAGE_ACCESS_TOKEN)
 
-@app.route('/webhook', methods=['GET'])
-def webhook_verify():
-    if request.args.get('hub.verify_token') == verify_token:
-        return request.args.get('hub.challenge')
-    return "Wrong verify token"
+VERIFY_TOKEN ='UNIQE_TOKEN'
+#################################################################################################
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'private_key.json'
 
-@app.route('/webhook', methods=['POST'])
-def webhook_action():
-    data = json.loads(request.data.decode('utf-8'))
-    for entry in data['entry']:
-        user_message = entry['messaging'][0]['message']['text']
-        user_id = entry['messaging'][0]['sender']['id']
-        response = {
-            'recipient': {'id': user_id},
-            'message': {}
-        }
-        response['message']['text'] = handle_message(user_id, user_message)
-        r = requests.post(
-            'https://graph.facebook.com/v2.6/me/messages/?access_token=' + access_token, json=response)
-    return Response(response="EVENT RECEIVED",status=200)
+DIALOGFLOW_PROJECT_ID = 'test-wj9j'
+DIALOGFLOW_LANGUAGE_CODE = 'fr'
+SESSION_ID = 'me'
+#########################################################################################################
 
-@app.route('/webhook_dev', methods=['POST'])
-def webhook_dev():
-    # custom route for local development
-    data = json.loads(request.data.decode('utf-8'))
-    user_message = data['entry'][0]['messaging'][0]['message']['text']
-    user_id = data['entry'][0]['messaging'][0]['sender']['id']
-    response = {
-        'recipient': {'id': user_id},
-        'message': {'text': handle_message(user_id, user_message)}
-    }
-    return Response(
-        response=json.dumps(response),
-        status=200,
-        mimetype='application/json'
-    )
+def process_message(text):
+    formatted_message = text.lower()
+    if formatted_message:
+        text_to_be_analyzed = formatted_message
+        session_client = dialogflow.SessionsClient()
+        session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+        text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        try:
+            response = session_client.detect_intent(session=session, query_input=query_input)
+        except InvalidArgument:
+            raise
+        response_sent_text = response.query_result.fulfillment_text
+        response = response_sent_text
+    return response
 
-def handle_message(user_id, user_message):
-    # DO SOMETHING with the user_message ... ¯\_(ツ)_/¯
-    return "Hello "+user_id+" ! You just sent me : " + user_message
+@app.route('/', methods=["POST", "GET"])
+def webhook():
+    if request.method == "GET":
+        if request.args.get("hub.verify_token") == VERIFY_TOKEN:
+            return request.args.get("hub.challenge")
+        else:
+            return "Hello"
 
-@app.route('/privacy', methods=['GET'])
-def privacy():
-    # needed route if you need to make your bot public
-    return "This facebook messenger bot's only purpose is to [...]. That's all. We don't use it in any other way."
+    elif request.method == "POST":
+        payload = request.json
+        event = payload['entry'][0]['messaging']
 
-@app.route('/', methods=['GET'])
-def index():
-    return "Hello there, I'm a facebook messenger bot."
+        for msg in event:
+            text = msg['message']['text']
+            sender_id = msg['sender']['id']
+
+            response = process_message(text)
+            bot.send_text_message(sender_id, response)
+
+        return "Message received"
+    else:
+        return "200"
+
+
+
 
 if __name__ == '__main__':
     app.run()

20dcc93f48dffe8b3ece95ec05f36fc4cd7056b4
agtmaroc <84507113+agtmaroc@users.noreply.github.com>
2021-06-01 00:41:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ VERIFY_TOKEN = 'UNIQE_TOKEN'
 bot = Bot(ACCESS_TOKEN)
 
 #################################################################################################
+private_key = json.loads(environ['credentials'])
+environ['GOOGLE_APPLICATION_CREDENTIALS'] ='private_key'
 DIALOGFLOW_PROJECT_ID = environ['DIALOGFLOW_PROJECT_ID']
 DIALOGFLOW_LANGUAGE_CODE = 'fr'
 SESSION_ID = environ['SESSION_ID']

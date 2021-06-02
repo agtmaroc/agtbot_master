f5653fd1cb92a3fd8d05a7a83a13dd6fce0cf0ac
agtmaroc <84507113+agtmaroc@users.noreply.github.com>
2021-06-02 11:26:33 +0100

    Update Conversations.py

--- saveConversation/Conversations.py
+++ saveConversation/Conversations.py
@@ -13,6 +13,12 @@ class Log:
         records.insert_one(mydict)
 
       
-
+    def saveInformation(self,userID,userName,userEmail,dbCon):
+        self.now = datetime.now()
+        self.date = self.now.sate()
+        self.current_time = self.now.strftime("%H:%M:%S")
+        mydict = {"userID":userID,"User": userName, "userEmail": userEmail, "Date": str(self.date) + "/" + str(self.current_time)}
+        records = dbConn.info_personne
+        records.insert_one(mydict)
 
    

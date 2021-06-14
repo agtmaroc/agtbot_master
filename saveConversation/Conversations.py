8a972beb3697566432566987730667e4e3d5f841
agtmaroc <84507113+agtmaroc@users.noreply.github.com>
2021-06-14 12:44:35 +0100

    Update Conversations.py

--- saveConversation/Conversations.py
+++ saveConversation/Conversations.py
@@ -4,7 +4,6 @@ class Log:
         pass
 
     def saveConversations(self, userID, usermessage,botmessage,dbConn):
-
         self.now = datetime.now()
         self.date = self.now.date()
         self.current_time = self.now.strftime("%H:%M:%S")
@@ -12,13 +11,14 @@ class Log:
         records = dbConn.info_conversation
         records.insert_one(mydict)
 
-      
-    def saveInformation(self,userID,userName,userEmail,dbConn):
+    def saveInformation(self,userID,nom,prenom,photo,dbConn):
         self.now = datetime.now()
-        self.date = self.now.sate()
+        self.date = self.now.date()
         self.current_time = self.now.strftime("%H:%M:%S")
-        mydict = {"userID":userID,"User": userName, "userEmail": userEmail, "Date": str(self.date) + "/" + str(self.current_time)}
+        mydict = {"userID":userID, "nom":nom, "prenom":prenom, "photo":photo, "Date": str(self.date) + "/" + str(self.current_time)}
         records = dbConn.info_personne
-        records.insert_one(mydict)
+        if(records.find({"userID":userID}).count() == 0):
+            records.insert_one(mydict)
+        else:
+            pass
 
-   

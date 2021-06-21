c16855adf86ff53d5bb90ab2972b1e4fe80c923d
agtmaroc <84507113+agtmaroc@users.noreply.github.com>
2021-06-21 15:05:59 +0100

    Update Conversations.py

--- saveConversation/Conversations.py
+++ saveConversation/Conversations.py
@@ -4,19 +4,14 @@ class Log:
         pass
 
     def saveConversations(self, userID, usermessage,botmessage,dbConn):
-
         self.now = datetime.now()
         self.date = self.now.date()
         self.current_time = self.now.strftime("%H:%M:%S")
-
-        #db = dbConn['Covid-19DB']  # connecting to the database called crawlerD
         mydict = {"userID":userID,"User": usermessage, "Bot": botmessage, "Date": str(self.date) + "/" + str(self.current_time)}
-
-        #table = db[sessionID]
         records = dbConn.info_conversation
         records.insert_one(mydict)
 
-        #table.insert_one(mydict)
+        
 
     def saveInformation(self,userID,nom,prenom,email,dbConn):
         self.now = datetime.now()

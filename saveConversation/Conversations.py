af672a94ef7bf68aef742380f87256ab082729cc
agtmaroc <84507113+agtmaroc@users.noreply.github.com>
2021-06-21 11:13:43 +0100

    Update Conversations.py

--- saveConversation/Conversations.py
+++ saveConversation/Conversations.py
@@ -4,21 +4,27 @@ class Log:
         pass
 
     def saveConversations(self, userID, usermessage,botmessage,dbConn):
+
         self.now = datetime.now()
         self.date = self.now.date()
         self.current_time = self.now.strftime("%H:%M:%S")
+
+        #db = dbConn['Covid-19DB']  # connecting to the database called crawlerD
         mydict = {"userID":userID,"User": usermessage, "Bot": botmessage, "Date": str(self.date) + "/" + str(self.current_time)}
-        records = dbConn.info_conversation
+
+        #table = db[sessionID]
+        records = dbConn.B
         records.insert_one(mydict)
 
-    def saveInformation(self,userID,nom,prenom,photo,dbConn):
+        #table.insert_one(mydict)
+
+    def saveInformation(self,userID,nom,prenom,email,dbConn):
         self.now = datetime.now()
         self.date = self.now.date()
         self.current_time = self.now.strftime("%H:%M:%S")
-        mydict = {"userID":userID, "nom":nom, "prenom":prenom, "photo":photo, "Date": str(self.date) + "/" + str(self.current_time)}
-        records = dbConn.info_personne
+        mydict = {"userID":userID, "nom":nom, "prenom":prenom, "email":email, "Date": str(self.date) + "/" + str(self.current_time)}
+        records = dbConn.A
         if(records.find({"userID":userID}).count() == 0):
             records.insert_one(mydict)
         else:
             pass
-
